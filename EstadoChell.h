e6c7e3a1afcb1202e24b45fe2a4f2e563875a762
Santiago <santiagomariani2@gmail.com>
2019-06-07 18:21:50 -0300

    Agrego animacion de chell

--- EstadoChell.h
+++ EstadoChell.h
@@ -11,6 +11,7 @@
 #define CHELL_EN_EL_AIRE 0
 #define CHELL_MOVIENDOSE 1
 #define CHELL_QUIETA 2
+#define CHELL_CAMBIA_ORIENTACION 3
 
 #define CHELL_MIRA_DERECHA 0
 #define CHELL_MIRA_IZQ 1
@@ -20,7 +21,7 @@ private:
     uint8_t estado_actual;
     uint8_t orientacion_actual;
 public:
-    EstadoChell() = default;
+    EstadoChell();
     EstadoChell(EstadoChell &&otro);
     void actualizarEstado(SensorChell &s, b2Vec2 &vel);
     uint8_t obtenerEstado();
--- ViewChell.h
+++ ViewChell.h
@@ -8,6 +8,7 @@
 
 #include <SDL2/SDL_system.h>
 #include <vector>
+#include <map>
 
 #include "SdlTexture.h"
 #include "Sprite.h"
@@ -15,16 +16,23 @@
 class ViewChell : public Renderable {
 private:
     SdlTexture chellTextures;
-    std::vector<Sprite> sprites;
+    std::map<uint8_t, Sprite> sprites;
     uint8_t estado_actual;
+    bool renderizando_estado_transitorio;
+    SDL_RendererFlip flip_transitorio;
+    double angulo_transitorio;
+    SDL_Point *centro_transitorio;
     void spritesCreation();
 public:
     explicit ViewChell(SdlWindow &window);
-    void cambiarEstado(uint8_t estado);
     void render(SDL_Rect &dest,
                      double angle,
                      SDL_Point *center,
                      SDL_RendererFlip flip) override;
+    void cambiarEstado(uint8_t estado,
+                       double angle = 0,
+                       SDL_Point *center = nullptr,
+                       SDL_RendererFlip flip = SDL_FLIP_NONE);
     ~ViewChell() = default;
 };
 
