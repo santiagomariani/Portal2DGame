e219f356511fe3783d5c301ed7d62ba20063e3a7
Santiago <santiagomariani2@gmail.com>
2019-06-17 19:01:34 -0300

    cambio botonees

--- libs/yaml-cpp/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ariel/Escritorio/Portal/TP4---Portal/libs/yaml-cpp/test
-# Build directory: /home/ariel/Escritorio/Portal/TP4---Portal/libs/yaml-cpp/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(yaml-test "/run-tests")
-set_tests_properties(yaml-test PROPERTIES  _BACKTRACE_TRIPLES "/home/ariel/Escritorio/Portal/TP4---Portal/libs/yaml-cpp/test/CMakeLists.txt;64;add_test;/home/ariel/Escritorio/Portal/TP4---Portal/libs/yaml-cpp/test/CMakeLists.txt;0;")
--- libs/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ariel/Escritorio/Portal/TP4---Portal/libs/yaml-cpp/util
-# Build directory: /home/ariel/Escritorio/Portal/TP4---Portal/libs/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
