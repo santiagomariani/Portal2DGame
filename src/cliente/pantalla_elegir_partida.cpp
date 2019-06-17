192acf54864950864d38a81d3baadb862e0f44a6
Santiago <santiagomariani2@gmail.com>
2019-06-17 18:25:58 -0300

    botones de seleccion

--- src/cliente/pantalla_elegir_partida.cpp
+++ src/cliente/pantalla_elegir_partida.cpp
@@ -9,6 +9,7 @@
 #include "boton_jugar.h"
 #include "boton_nueva_partida.h"
 #include "boton_unirse_a_partida.h"
+#include "fondo_continuo.h"
 
 
 PantallaElegirPartida::PantallaElegirPartida(std::string puerto,
@@ -37,24 +38,32 @@ bool PantallaElegirPartida::operator()(){
 
     Ventana ventana(ancho_pantalla, alto_pantalla);
 
-
     //=Fondo=
     std::string ruta_fondo = "assets/inicio5.png";
     Textura textura_fondo(ruta_fondo, ventana);
 
+    FondoContinuo fondo(1800, 1100, ancho_pantalla, alto_pantalla, 3, textura_fondo);
+    //=Titulo=
+    std::string  ruta_titulo = "assets/titulo.png";
+    Textura titulo_textura(ruta_titulo, ventana);
+    Imagen titulo(0, 0, 470, 200, &titulo_textura);
+    SDL_Rect destino_titulo = {(ancho_pantalla / 2) - (470 / 2), (alto_pantalla / 3) - 100, 470, 200};
+
+
     bool corriendo = true;
 
     //=Botones=
-    std::string  ruta_botones = "assets/botones_inicio.png";
-    Textura botones(ruta_botones, ventana);
+    std::string  ruta_botones_seleccion = "assets/botones_seleccion.png";
+    Textura botones(ruta_botones_seleccion, ventana);
 
-    Imagen imagen_nueva_partida(64, 71, 177, 81, &botones); // nueva partida
-    BotonNuevaPartida nueva_partida(&imagen_nueva_partida, &corriendo, protocolo_opcion, puerto);
+
+    Imagen imagen_crear(64, 71, 177, 81, &botones); // nueva partida
+    BotonNuevaPartida nueva_partida(&imagen_crear, &corriendo, protocolo_opcion, puerto);
     nueva_partida.colocar((ancho_pantalla / 2) - (177 / 2), (alto_pantalla * 3 / 5) - 40, 177, 81);
 
     //Falta boton unirse a partida
-    Imagen imagen_editar(65, 226, 177, 81, &botones);
-    BotonUnirseAPartida unirse_partida(&imagen_editar, &corriendo, protocolo_opcion, puerto);
+    Imagen imagen_unirse(65, 226, 177, 81, &botones);
+    BotonUnirseAPartida unirse_partida(&imagen_unirse, &corriendo, protocolo_opcion, puerto);
     unirse_partida.colocar((ancho_pantalla / 2) - (177 / 2), (alto_pantalla * 3 / 5) + 60, 177, 81);
 
 
@@ -65,13 +74,13 @@ bool PantallaElegirPartida::operator()(){
     while (corriendo){
 
         ventana.pintar(0x33, 0x33, 0x33, 0xFF);
-
+        fondo.render();
+        titulo.render(destino_titulo);
         nueva_partida.render();
         unirse_partida.render();
-
         ventana.renderizar();
 
-        while (SDL_PollEvent(&event) != 0){
+        while (SDL_PollEvent(&event) != 0) {
             switch(event.type) {
                 case SDL_MOUSEBUTTONDOWN:{
                     SDL_MouseButtonEvent& mouseEvent = (SDL_MouseButtonEvent&) event;
